a06957a3267c48c9a51593983139ac646323a397
Tsuyoshi Inoue <inoue0406@gmail.com>
2023-09-27 10:03:34 +0900

    Vit Tutorial

---
+++ src/vit-tutorical.py
@@ -0,0 +1,11 @@
+
+# This is a follow-up of ViT in pytorch lightning
+# https://lightning.ai/docs/pytorch/stable/notebooks/course_UvA-DL/11-vision-transformer.html
+
+import os
+import urllib.request
+from urllib.error import HTTPError
+
+import lightning as L
+
+import pdb;pdb.set_trace()
