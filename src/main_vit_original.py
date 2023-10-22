173119af6b003832f62b3486b72a4c2aa96bfc0d
Tsuyoshi Inoue <inoue0406@gmail.com>
2023-10-23 02:15:28 +0900

    ViT main routine modified.

--- src/main_vit_original.py
+++ src/main_vit_original.py
@@ -26,109 +26,6 @@ from torchvision.datasets import CIFAR10
 # local modules
 from models.model_ViT import img_to_patch, ViT
 
-plt.set_cmap("cividis")
-matplotlib.rcParams["lines.linewidth"] = 2.0
-sns.reset_orig()
-
-# Path to the folder where the datasets are/should be downloaded (e.g. CIFAR10)
-DATASET_PATH = os.environ.get("PATH_DATASETS", "../data/")
-# Path to the folder where the pretrained models are saved
-CHECKPOINT_PATH = os.environ.get("PATH_CHECKPOINT", "../run/saved_models/VisionTransformers/")
-
-# Setting the seed
-L.seed_everything(42)
-
-# Ensure that all operations are deterministic on GPU (if used) for reproducibility
-torch.backends.cudnn.deterministic = True
-torch.backends.cudnn.benchmark = False
-
-device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
-print("Device:", device)
-
-# Github URL where saved models are stored for this tutorial
-base_url = "https://raw.githubusercontent.com/phlippe/saved_models/main/"
-# Files to download
-pretrained_files = [
-    "tutorial15/ViT.ckpt",
-    "tutorial15/tensorboards/ViT/events.out.tfevents.ViT",
-    "tutorial5/tensorboards/ResNet/events.out.tfevents.resnet",
-]
-# Create checkpoint path if it doesn't exist yet
-os.makedirs(CHECKPOINT_PATH, exist_ok=True)
-
-# For each file, check whether it already exists. If not, try downloading it.
-for file_name in pretrained_files:
-    file_path = os.path.join(CHECKPOINT_PATH, file_name.split("/", 1)[1])
-    if "/" in file_name.split("/", 1)[1]:
-        os.makedirs(file_path.rsplit("/", 1)[0], exist_ok=True)
-    if not os.path.isfile(file_path):
-        file_url = base_url + file_name
-        print("Downloading %s..." % file_url)
-        try:
-            urllib.request.urlretrieve(file_url, file_path)
-        except HTTPError as e:
-            print(
-                "Something went wrong. Please try to download the file from the GDrive folder, or contact the author with the full output including the following error:\n",
-                e,
-            )
-
-test_transform = transforms.Compose(
-    [
-        transforms.ToTensor(),
-        transforms.Normalize([0.49139968, 0.48215841, 0.44653091], [0.24703223, 0.24348513, 0.26158784]),
-    ]
-)
-# For training, we add some augmentation. Networks are too powerful and would overfit.
-train_transform = transforms.Compose(
-    [
-        transforms.RandomHorizontalFlip(),
-        transforms.RandomResizedCrop((32, 32), scale=(0.8, 1.0), ratio=(0.9, 1.1)),
-        transforms.ToTensor(),
-        transforms.Normalize([0.49139968, 0.48215841, 0.44653091], [0.24703223, 0.24348513, 0.26158784]),
-    ]
-)
-# Loading the training dataset. We need to split it into a training and validation part
-# We need to do a little trick because the validation set should not use the augmentation.
-train_dataset = CIFAR10(root=DATASET_PATH, train=True, transform=train_transform, download=True)
-val_dataset = CIFAR10(root=DATASET_PATH, train=True, transform=test_transform, download=True)
-L.seed_everything(42)
-train_set, _ = torch.utils.data.random_split(train_dataset, [45000, 5000])
-L.seed_everything(42)
-_, val_set = torch.utils.data.random_split(val_dataset, [45000, 5000])
-
-# Loading the test set
-test_set = CIFAR10(root=DATASET_PATH, train=False, transform=test_transform, download=True)
-
-# We define a set of data loaders that we can use for various purposes later.
-train_loader = data.DataLoader(train_set, batch_size=128, shuffle=True, drop_last=True, pin_memory=True, num_workers=4)
-val_loader = data.DataLoader(val_set, batch_size=128, shuffle=False, drop_last=False, num_workers=4)
-test_loader = data.DataLoader(test_set, batch_size=128, shuffle=False, drop_last=False, num_workers=4)
-
-# Visualize some examples
-NUM_IMAGES = 4
-CIFAR_images = torch.stack([val_set[idx][0] for idx in range(NUM_IMAGES)], dim=0)
-img_grid = torchvision.utils.make_grid(CIFAR_images, nrow=4, normalize=True, pad_value=0.9)
-img_grid = img_grid.permute(1, 2, 0)
-
-plt.figure(figsize=(8, 8))
-plt.title("Image examples of the CIFAR10 dataset")
-plt.imshow(img_grid)
-plt.axis("off")
-plt.show()
-plt.close()
-
-img_patches = img_to_patch(CIFAR_images, patch_size=4, flatten_channels=False)
-
-fig, ax = plt.subplots(CIFAR_images.shape[0], 1, figsize=(14, 3))
-fig.suptitle("Images as input sequences of patches")
-for i in range(CIFAR_images.shape[0]):
-    img_grid = torchvision.utils.make_grid(img_patches[i], nrow=64, normalize=True, pad_value=0.9)
-    img_grid = img_grid.permute(1, 2, 0)
-    ax[i].imshow(img_grid)
-    ax[i].axis("off")
-plt.show()
-plt.close()
-        
 def train_model(**kwargs):
     trainer = L.Trainer(
         default_root_dir=os.path.join(CHECKPOINT_PATH, "ViT"),
@@ -167,19 +64,124 @@ def train_model(**kwargs):
     return model, result
 #import pdb;pdb.set_trace()
 
-model, results = train_model(
-    model_kwargs={
-        "embed_dim": 256,
-        "hidden_dim": 512,
-        "num_heads": 8,
-        "num_layers": 6,
-        "patch_size": 4,
-        "num_channels": 3,
-        "num_patches": 64,
-        "num_classes": 10,
-        "dropout": 0.2,
-    },
-    lr=3e-4,
-)
-print("ViT results", results)
+if __name__ == '__main__':
+
+    plt.set_cmap("cividis")
+    matplotlib.rcParams["lines.linewidth"] = 2.0
+    sns.reset_orig()
+
+    # Path to the folder where the datasets are/should be downloaded (e.g. CIFAR10)
+    DATASET_PATH = os.environ.get("PATH_DATASETS", "../data/")
+    # Path to the folder where the pretrained models are saved
+    CHECKPOINT_PATH = os.environ.get("PATH_CHECKPOINT", "../run/saved_models/VisionTransformers/")
+
+    # Setting the seed
+    L.seed_everything(42)
+
+    # Ensure that all operations are deterministic on GPU (if used) for reproducibility
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+    device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
+    print("Device:", device)
+
+    # Github URL where saved models are stored for this tutorial
+    base_url = "https://raw.githubusercontent.com/phlippe/saved_models/main/"
+    # Files to download
+    pretrained_files = [
+        "tutorial15/ViT.ckpt",
+        "tutorial15/tensorboards/ViT/events.out.tfevents.ViT",
+        "tutorial5/tensorboards/ResNet/events.out.tfevents.resnet",
+    ]
+    # Create checkpoint path if it doesn't exist yet
+    os.makedirs(CHECKPOINT_PATH, exist_ok=True)
+
+    # For each file, check whether it already exists. If not, try downloading it.
+    for file_name in pretrained_files:
+        file_path = os.path.join(CHECKPOINT_PATH, file_name.split("/", 1)[1])
+        if "/" in file_name.split("/", 1)[1]:
+            os.makedirs(file_path.rsplit("/", 1)[0], exist_ok=True)
+        if not os.path.isfile(file_path):
+            file_url = base_url + file_name
+            print("Downloading %s..." % file_url)
+            try:
+                urllib.request.urlretrieve(file_url, file_path)
+            except HTTPError as e:
+                print(
+                    "Something went wrong. Please try to download the file from the GDrive folder, or contact the author with the full output including the following error:\n",
+                    e,
+                )
+
+    test_transform = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize([0.49139968, 0.48215841, 0.44653091], [0.24703223, 0.24348513, 0.26158784]),
+        ]
+    )
+    # For training, we add some augmentation. Networks are too powerful and would overfit.
+    train_transform = transforms.Compose(
+        [
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomResizedCrop((32, 32), scale=(0.8, 1.0), ratio=(0.9, 1.1)),
+            transforms.ToTensor(),
+            transforms.Normalize([0.49139968, 0.48215841, 0.44653091], [0.24703223, 0.24348513, 0.26158784]),
+        ]
+    )
+    # Loading the training dataset. We need to split it into a training and validation part
+    # We need to do a little trick because the validation set should not use the augmentation.
+    train_dataset = CIFAR10(root=DATASET_PATH, train=True, transform=train_transform, download=True)
+    val_dataset = CIFAR10(root=DATASET_PATH, train=True, transform=test_transform, download=True)
+    L.seed_everything(42)
+    train_set, _ = torch.utils.data.random_split(train_dataset, [45000, 5000])
+    L.seed_everything(42)
+    _, val_set = torch.utils.data.random_split(val_dataset, [45000, 5000])
+
+    # Loading the test set
+    test_set = CIFAR10(root=DATASET_PATH, train=False, transform=test_transform, download=True)
+
+    # We define a set of data loaders that we can use for various purposes later.
+    train_loader = data.DataLoader(train_set, batch_size=128, shuffle=True, drop_last=True, pin_memory=True, num_workers=4)
+    val_loader = data.DataLoader(val_set, batch_size=128, shuffle=False, drop_last=False, num_workers=4)
+    test_loader = data.DataLoader(test_set, batch_size=128, shuffle=False, drop_last=False, num_workers=4)
+
+    # Visualize some examples
+    NUM_IMAGES = 4
+    CIFAR_images = torch.stack([val_set[idx][0] for idx in range(NUM_IMAGES)], dim=0)
+    img_grid = torchvision.utils.make_grid(CIFAR_images, nrow=4, normalize=True, pad_value=0.9)
+    img_grid = img_grid.permute(1, 2, 0)
+
+    plt.figure(figsize=(8, 8))
+    plt.title("Image examples of the CIFAR10 dataset")
+    plt.imshow(img_grid)
+    plt.axis("off")
+    plt.show()
+    plt.close()
+
+    img_patches = img_to_patch(CIFAR_images, patch_size=4, flatten_channels=False)
+
+    fig, ax = plt.subplots(CIFAR_images.shape[0], 1, figsize=(14, 3))
+    fig.suptitle("Images as input sequences of patches")
+    for i in range(CIFAR_images.shape[0]):
+        img_grid = torchvision.utils.make_grid(img_patches[i], nrow=64, normalize=True, pad_value=0.9)
+        img_grid = img_grid.permute(1, 2, 0)
+        ax[i].imshow(img_grid)
+        ax[i].axis("off")
+    plt.show()
+    plt.close()
+
+    model, results = train_model(
+        model_kwargs={
+            "embed_dim": 256,
+            "hidden_dim": 512,
+            "num_heads": 8,
+            "num_layers": 6,
+            "patch_size": 4,
+            "num_channels": 3,
+            "num_patches": 64,
+            "num_classes": 10,
+            "dropout": 0.2,
+        },
+        lr=3e-4,
+    )
+    print("ViT results", results)
 
